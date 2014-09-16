2d9c915ce3c690d9dfb52018a3291880e496f520
Jamie (Lexteam) <lexwaresupport@outlook.com>
2014-09-16 20:47:08 +0100

    Added multiple server support, still needs more work done.

--- src/main/java/io/github/asyncronous/cube/obj/Server.java
+++ src/main/java/io/github/asyncronous/cube/obj/Server.java
@@ -1,25 +1,40 @@
 /**
  * Copyright (C) 2014 Asyncronous
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
-package io.github.asyncronous.cube.data;
+package io.github.asyncronous.cube.obj;
 
-import io.github.asyncronous.cube.obj.Server;
-import io.github.asyncronous.cube.obj.ServerList;
+public class Server{
+    private String name;
+    private String url;
+    private boolean defaultSelected;
 
-public final class Constants{
-    public static final ServerList SERVERS = new ServerList(new Server[] { new Server("Auto", "http://theenderchest.com/", true) });
+    public Server(String name, String url, boolean defaultSelected){
+        this.name = name;
+        this.url = url;
+        this.defaultSelected = defaultSelected;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public String getURL(){
+        return url;
+    }
+    public boolean isDefaultSelected(){
+        return defaultSelected;
+    }
 }
--- src/main/java/io/github/asyncronous/cube/obj/ServerList.java
+++ src/main/java/io/github/asyncronous/cube/obj/ServerList.java
@@ -1,25 +1,35 @@
 /**
  * Copyright (C) 2014 Asyncronous
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
-package io.github.asyncronous.cube.data;
+package io.github.asyncronous.cube.obj;
 
-import io.github.asyncronous.cube.obj.Server;
-import io.github.asyncronous.cube.obj.ServerList;
+public class ServerList {
+    private Server[] serverList;
 
-public final class Constants{
-    public static final ServerList SERVERS = new ServerList(new Server[] { new Server("Auto", "http://theenderchest.com/", true) });
+    public ServerList(Server[] serverList){
+        this.serverList = serverList;
+    }
+
+    public Server getDefault(){
+        for(Server server : serverList){
+            if(server.isDefaultSelected()){
+                return server;
+            }
+        }
+        return serverList[0];
+    }
 }
